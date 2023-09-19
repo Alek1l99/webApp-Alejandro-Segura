5e98db791abb90854beba58cb812f65c3dcd9555
Alek1l99 <miguelsegura618@gmail.com>
2023-09-19 18:59:01 -0500

    Mejoras en la organización del código y traduccion de texto

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,5 +1,6 @@
+// Importaciones de módulos y datos necesarios
 import { Component, OnInit, HostListener } from '@angular/core';
-import {backgoundList, images} from 'src/app/data/dataListMedia';
+import { backgoundList, images } from 'src/app/data/dataListMedia';
 
 @Component({
   selector: 'app-profile',
@@ -8,22 +9,24 @@ import {backgoundList, images} from 'src/app/data/dataListMedia';
 })
 export class ProfileComponent implements OnInit {
 
-  background = backgoundList ;
-  images = images;
+  // Declaración de propiedades del componente
+  background = backgoundList; // Una lista de fondos
+  images = images; // Una lista de imágenes
 
-  heightDevice:number;
+  heightDevice: number; // Una propiedad para almacenar la altura de la ventana del navegador
 
+  // Constructor del componente
   constructor() {
-    this.heightDevice = window.innerHeight;
-   }
-
-   @HostListener('window:resize', ['$event'])
-   onResize(event: any) {
-     this.heightDevice = event.target.innerHeight;
-   }
-
-  ngOnInit(): void {
+    this.heightDevice = window.innerHeight; // Inicializa la propiedad 'heightDevice' con la altura actual de la ventana
   }
 
-}
+  // Manejador de eventos que se dispara cuando se redimensiona la ventana del navegador
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.heightDevice = event.target.innerHeight; // Actualiza la propiedad 'heightDevice' cuando se produce un cambio de tamaño en la ventana
+  }
 
+  ngOnInit(): void {
+    // ngOnInit es un método que se llama una vez que el componente se ha inicializado, pero en este caso no hace nada.
+  }
+}
